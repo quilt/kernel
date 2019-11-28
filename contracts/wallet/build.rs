253733190dffa059cb9ba17a2dab059d49e33c85
Matt Garnett <matt.garnett@me.com>
2019-11-28 02:14:55 -0700

    stop build if build script fails

--- contracts/wallet/build.rs
+++ contracts/wallet/build.rs
@@ -8,10 +8,12 @@ fn main() {
     let binary_name = Path::new("main.rs");
     let input_path = Path::new(&manifest_dir).join("src").join(binary_name);
 
-    Command::new(env::var("RUSTC").unwrap())
+    let status = Command::new(env::var("RUSTC").unwrap())
         .arg("--target=wasm32-unknown-unknown")
         .arg(format!("--out-dir={}", out_dir))
         .arg(format!("{}", input_path.to_string_lossy()))
-        .spawn()
-        .expect("rustc to execute");
+        .status()
+        .expect("failed to execute rustc");
+
+    assert!(status.success());
 }
