10d16b81e806e0fe1efd6fb1ac29b67462541541
Matt Garnett <matt.garnett@me.com>
2019-11-28 19:12:13 -0700

    contract runs via integration test

--- contracts/wallet/tests/executes.rs
+++ contracts/wallet/tests/executes.rs
@@ -1,6 +1,13 @@
+use ewasm::{Execute, RootRuntime};
 use wallet::asm;
 
 #[test]
 fn test() {
-    println!("binary size: {}", asm().len());
+    let mut runtime = RootRuntime::new(&asm(), &[], [0u8; 32]);
+
+    runtime.set_logger(|b| {
+        println!("{}", b);
+    });
+
+    let _ = runtime.execute();
 }
